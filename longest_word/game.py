6e52a76f8c7b29fe7d85134e6ebec5ab7d49d520
arnaud longueval <arnaud.longueval@edhec.com>
2022-12-20 14:01:09 +0000

    Feature complete: Dictionary check of attempt, PLS ROMAIN REVIEW that awesome code

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,5 +1,7 @@
 import random
 import string
+import requests
+import json
 
 
 class Game:
@@ -12,10 +14,22 @@ class Game:
         """Return True if and only if the word is valid, given the Game's grid"""
         if not word:
             return False
+
         letters = self.grid.copy() # Consume letters from the grid
         for letter in word:
             if letter in letters:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
+
+
+new_game = Game()
+new_game.grid = list('KWIENFUQW')
+new_game.is_valid('FEUN')
